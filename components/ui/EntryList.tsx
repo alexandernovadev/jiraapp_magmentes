6fd3c42e38f2fcc98e583122117804b9ec0d193d
alexandernovadev <alexsk88.dev@gmail.com>
2023-07-23 14:51:51 -0500

    Lonter

--- components/ui/EntryList.tsx
+++ components/ui/EntryList.tsx
@@ -1,58 +1,61 @@
-import { FC, useContext, useMemo, DragEvent } from 'react';
-import { List, Paper } from '@mui/material';
+import { FC, useContext, useMemo, DragEvent } from "react";
+import { List, Paper } from "@mui/material";
 
-import { EntriesContext } from '../../context/entries';
-import { UIContext } from '../../context/ui';
+import { EntriesContext } from "../../context/entries";
+import { UIContext } from "../../context/ui";
 
-import { EntryStatus } from '../../interfaces';
-import { EntryCard } from './';
+import { EntryStatus } from "../../interfaces";
+import { EntryCard } from "./";
 
-import styles from './EntryList.module.css';
+import styles from "./EntryList.module.css";
 
 interface Props {
-    status: EntryStatus;
+  status: EntryStatus;
 }
 
+export const EntryList: FC<Props> = ({ status }) => {
+  const { entries, updateEntry } = useContext(EntriesContext);
+  const { isDragging, endDragging } = useContext(UIContext);
 
-export const EntryList:FC<Props> = ({ status }) => {
+  const entriesByStatus = useMemo(
+    () => entries.filter((entry) => entry.status === status),
+    [entries, status]
+  );
 
-    const { entries, updateEntry } = useContext( EntriesContext );
-    const { isDragging, endDragging } = useContext( UIContext );
+  const allowDrop = (event: DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+  };
 
-    const entriesByStatus = useMemo( () => entries.filter( entry => entry.status === status ), [ entries ]);
+  const onDropEntry = (event: DragEvent<HTMLDivElement>) => {
+    const id = event.dataTransfer.getData("text");
 
-    const allowDrop = ( event: DragEvent<HTMLDivElement> ) => {
-        event.preventDefault();
-    }
+    const entry = entries.find((e) => e._id === id)!;
+    entry.status = status;
+    updateEntry(entry);
+    endDragging();
+  };
 
-    const onDropEntry = ( event: DragEvent<HTMLDivElement> ) => {
-        const id = event.dataTransfer.getData('text');
-        
-        const entry = entries.find( e => e._id === id )!;
-        entry.status = status;
-        updateEntry( entry );
-        endDragging();
-    }
-
-      
-    return (
-        //   TODO: aquí haremos drop
-        <div
-            onDrop={ onDropEntry }
-            onDragOver={ allowDrop }
-            className={ isDragging ? styles.dragging : '' }
-        >
-            <Paper sx={{ height: 'calc(100vh - 180px)', overflow: 'scroll', backgroundColor: 'transparent', padding: '3px 5px'  }}>
-
-                <List sx={{ opacity: isDragging ? 0.2 : 1, transition: 'all .3s' }}> 
-                    {
-                        entriesByStatus.map( entry => (
-                            <EntryCard key={ entry._id } entry={ entry } />
-                        ))
-                    }
-                </List>
-
-            </Paper>
-        </div>
-    )
+  return (
+    //   TODO: aquí haremos drop
+    <div
+      onDrop={onDropEntry}
+      onDragOver={allowDrop}
+      className={isDragging ? styles.dragging : ""}
+    >
+      <Paper
+        sx={{
+          height: "calc(100vh - 180px)",
+          overflow: "scroll",
+          backgroundColor: "transparent",
+          padding: "3px 5px",
+        }}
+      >
+        <List sx={{ opacity: isDragging ? 0.2 : 1, transition: "all .3s" }}>
+          {entriesByStatus.map((entry) => (
+            <EntryCard key={entry._id} entry={entry} />
+          ))}
+        </List>
+      </Paper>
+    </div>
+  );
 };
